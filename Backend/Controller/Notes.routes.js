4a5b0edf8b9c8492d78c60830618db18ea31b216
Imran Khan <99397606+imran120198@users.noreply.github.com>
2023-01-31 19:20:39 +0530

    backend

--- Backend/Controller/Notes.routes.js
+++ Backend/Controller/Notes.routes.js
@@ -5,11 +5,25 @@ const { NotesModel } = require("../Models/Notes.model");
 
 const notesController = Router();
 
-notesController.get("/", (req, res) => {
-  res.send("Notes");
+notesController.get("/", async (req, res) => {
+  const notes = await NotesModel.find({ _id: req.body.userId });
+  res.send(notes);
 });
 
- 
+notesController.post("/create", async (req, res) => {
+  const { Heading, Notes, Tag } = req.body;
+  const notes = new NotesModel({
+    Heading,
+    Notes,
+    Tag,
+  });
+  try {
+    await notes.save();
+    res.send("notes created");
+  } catch (err) {
+    res.send("Something went wrong");
+  }
+});
 
 module.exports = {
   notesController,
