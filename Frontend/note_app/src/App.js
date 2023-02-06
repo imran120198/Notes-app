01f1a16e059e32001926c3e76fff719045165cbb
Imran Khan <99397606+imran120198@users.noreply.github.com>
2023-02-06 10:36:16 +0530

    Frontend

--- Frontend/note_app/src/App.js
+++ Frontend/note_app/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import {Home} from "./Components/Home"
+import {Login} from "./Components/Login"
+import {Register} from "./Components/Register"
+import { Notes } from './Components/Notes';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/register" element={<Register />}/>
+            <Route path="/login" element={<Login />}/>
+            <Route path="/notes" element={<Notes />}/>
+        </Routes>
     </div>
   );
 }
